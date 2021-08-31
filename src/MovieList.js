db0c47b2494f6c805bd4a06cabadd99a45120655
Jimmy Pham <jimmyp.dev3@gmail.com>
2021-08-31 14:32:30 -0700

    small changes

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,18 +2,21 @@ import React from 'react'
 import Movie from './Movie'
 
 export default function MovieList(movies) {
-    console.log(movies);
-    const movieList = Array.from(movies);
+    console.log(movies.movies);
+    // const movieList = Array.from(movies);
+    
 
     
     return ( 
-        movieList.map(movie => {
-            return (
-                <div> 
-                    <h1>This is a movie</h1>
-            <Movie title={movie.title} year={movie.year} genre={movie.genre} />
-            </div>
-            )
-        })
-    );
+        <div>
+            {movies.movies.map(movie => {
+                return (
+                    <div> 
+                        <h1>This is a movie</h1>
+                <Movie title={movie.title} year={movie.year} genre={movie.genre} />
+                </div>
+                )
+            })}
+        </div>
+    )
 }
